a55316d339c8b9119dd690cc79b924ea2773e2a4
Jung, SungOh <tryjso@gmail.com>
2024-03-04 10:40:33 +0900

    first commit

---
+++ src/main/java/com/koreatech/lightup/LightupApplication.java
@@ -0,0 +1,13 @@
+package com.koreatech.lightup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LightupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LightupApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/koreatech/lightup/LightupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koreatech.lightup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LightupApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
