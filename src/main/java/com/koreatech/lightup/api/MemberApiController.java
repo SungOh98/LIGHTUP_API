452865f6f30fb410573e23f893c305854e86c65d
Jung, SungOh <tryjso@gmail.com>
2024-03-10 22:41:14 +0900

    Feat: 회원 팔로우 기능
    회원 팔로우 API 생성

--- src/main/java/com/koreatech/lightup/api/MemberApiController.java
+++ src/main/java/com/koreatech/lightup/api/MemberApiController.java
@@ -3,6 +3,8 @@ package com.koreatech.lightup.api;
 import com.koreatech.lightup.service.MemberService;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @RestController
@@ -20,8 +23,20 @@ public class MemberApiController {
 
     @PostMapping("/following")
     public ResponseEntity<Map<String, String>> following(@RequestBody FollowingDto followingDto) {
-        memberService.follow(followingDto.getFollowerId(), followingDto.getFolloweeName());
-        return null;
+        Map<String, String> resultMap = new HashMap<>();
+        HttpStatus httpStatus;
+        try {
+            memberService.follow(followingDto.getFollowerId(), followingDto.getFolloweeName());
+            resultMap.put("message", "success!");
+            httpStatus = HttpStatus.OK;
+
+        }catch (Exception e) {
+            resultMap.put("message", "FAILED!!");
+            httpStatus = HttpStatus.INTERNAL_SERVER_ERROR;
+        }
+
+        return new ResponseEntity<>(resultMap, httpStatus);
+
     }
 
     @Data
