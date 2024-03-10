d75f80c558f9a64e860b900ad499358319c1a92a
Jung, SungOh <tryjso@gmail.com>
2024-03-10 21:58:21 +0900

    Make other changes

---
+++ src/main/java/com/koreatech/lightup/api/FollowController.java
@@ -0,0 +1,9 @@
+package com.koreatech.lightup.api;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class FollowController {
+}
